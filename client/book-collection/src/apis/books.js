4328f4e4965fd7f304bfaba4022f1aa3c289f6cf
Atchariya <atchariya.nna@gmail.com>
2025-12-06 10:20:27 +0000

    fix: fix incorrect code on books.js FE

--- client/book-collection/src/apis/books.js
+++ client/book-collection/src/apis/books.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base_url = import.meta.resolve.env.VITE_API_URL;
+const base_url = import.meta.env.VITE_API_URL;
 const endPoint = base_url + "/books"
 export async function getBooks (clientData) {
     try {
