095c97fdcf6f787e919eec7d24137521645abc98
Atchariya <atchariya.nna@gmail.com>
2026-02-12 16:43:31 +0000

    fix(prisma): pass datasourceUrl to PrismaClient for Vercel runtime

--- server/prisma/client.js
+++ server/prisma/client.js
@@ -2,11 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
+const options = {
+  datasourceUrl: process.env.DATABASE_URL,
+};
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient(options);
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient(options);
   }
   prisma = global.prisma;
 }
